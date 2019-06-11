55a433f504122799e3f5873a1077d1b656f332a6
renyong <497269678@qq.com>
2019-06-11 20:54:46 +0800

    first commit

---
+++ src/main/java/elk/Main.java
@@ -0,0 +1,21 @@
+package elk;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * *************书山有路勤为径***************
+ * 鲁班学院
+ * 往期资料加木兰老师  QQ: 2746251334
+ * VIP课程加安其拉老师 QQ: 3164703201
+ * 讲师：周瑜老师
+ * *************学海无涯苦作舟***************
+ */
+public class Main {
+
+    final static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("elk logs");
+    }
+}
